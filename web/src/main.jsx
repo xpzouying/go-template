76b19a3fecaf969276355107e5f98549f7e3b5aa
zy <xpzouying@gmail.com>
2024-01-28 23:53:32 +0800

    add destroy contact

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -11,6 +11,9 @@ import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, {
   action as editAction,
 } from "./routes/edit";
+import {
+  action as destroyAction,
+} from "./routes/destroy";
 
 const router = createBrowserRouter([
   {
@@ -31,6 +34,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
       },
     ],
   },

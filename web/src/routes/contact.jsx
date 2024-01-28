74a579a61dcebe4040197c777ad0bcd8991a41c0
zy <xpzouying@gmail.com>
2024-01-28 23:53:32 +0800

    add destroy contact

--- web/src/routes/contact.jsx
+++ web/src/routes/contact.jsx
@@ -40,20 +40,27 @@ export default function Contact() {
         {contact.notes && <p>{contact.notes}</p>}
 
         <div>
+
           <Form action="edit">
             <button type="submit">Edit</button>
           </Form>
+
           <Form
             method="post"
             action="destroy"
             onSubmit={(event) => {
-              if (!confirm("Please confirm you want to delete this record.")) {
+              if (
+                !confirm(
+                  "Please confirm you want to delete this record."
+                )
+              ) {
                 event.preventDefault();
               }
             }}
           >
             <button type="submit">Delete</button>
           </Form>
+
         </div>
       </div>
     </div>

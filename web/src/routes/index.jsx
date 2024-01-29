e59c5cd331f5e926a7351e050f16a08de393b872
zy <xpzouying@gmail.com>
2024-01-29 10:52:35 +0800

    add tailwindcss

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,10 +1,7 @@
 export default function Index() {
   return (
-    <p id="zero-state">
-      This is a demo for React Router.
-      <br />
-      Check out{" "}
-      <a href="https://reactrouter.com">the docs at reactrouter.com</a>.
-    </p>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   );
 }

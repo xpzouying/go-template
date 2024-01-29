7b26f033c37f8befb7a4b79ddd533a215063b0be
zy <xpzouying@gmail.com>
2024-01-29 12:55:19 +0800

    add shadcn/ui

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button"
+
+
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div>
+        <Button>Click me</Button>
+      </div>
+
+    </>
   );
 }

dc6b5969981981a0a9636a19ab81df01997f6ade
zy <xpzouying@gmail.com>
2024-01-30 11:27:46 +0800

    add shadcn ui component

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,67 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import Root, {
-  loader as rootLoader,
-  action as rootAction,
-} from "./routes/root";
-import ErrorPage from "./error-page";
-import Contact, {
-  loader as contactLoader,
-  action as contactAction,
-} from "./routes/contact";
-import EditContact, {
-  action as editAction,
-} from "./routes/edit";
-import {
-  action as destroyAction,
-} from "./routes/destroy";
-import Index from "./routes/index";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      {
-        errorElement: <ErrorPage />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-
-          {
-            path: "contacts/:contactId",
-            element: <Contact />,
-            loader: contactLoader,
-            action: contactAction,
-          },
-
-          {
-            path: "contacts/:contactId/edit",
-            element: <EditContact />,
-            loader: contactLoader,
-            action: editAction,
-          },
-
-          {
-            path: "contacts/:contactId/destroy",
-            action: destroyAction,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
-);
+)

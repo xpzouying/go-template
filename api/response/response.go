b360d50648f310b57d499b2209ef6de9d8302a1a
zy <xpzouying@gmail.com>
2024-01-31 21:31:14 +0800

    backend: init go http server

---
+++ api/response/response.go
@@ -0,0 +1,28 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Success bool        `json:"result"`
+	Error   string      `json:"error,omitempty"`
+	Data    interface{} `json:"data"`
+}
+
+// WriteSuccess writes a success response.
+func WriteSuccess(c *gin.Context, data interface{}) {
+
+	c.JSON(http.StatusOK,
+		Response{
+			Success: true,
+			Data:    data,
+		})
+}
+
+type StatusResult struct {
+	Version   string `json:"version"`
+	StartTime string `json:"start_time"`
+}

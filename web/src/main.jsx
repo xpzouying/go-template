fc8ddfd25ab7dcfa5b4cdccd438927bda090169c
zy <xpzouying@gmail.com>
2024-01-31 18:46:00 +0800

    add react route

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Root from "./routes/root";
+import About from './pages/About';
+import Index from './pages/Index';
+import Contact from './routes/contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

0206e1bf08644e53bde9b0162b2bedc5b0725052
zy <xpzouying@gmail.com>
2024-02-03 01:23:45 +0800

    request backend for get status

--- web/vite.config.js
+++ web/vite.config.js
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    proxy: {
+      "/status": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 })

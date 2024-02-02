1936a4fec8707712333af206db42c60a30f345af
zy <xpzouying@gmail.com>
2024-02-03 01:23:45 +0800

    request backend for get status

--- web/src/pages/Index.jsx
+++ web/src/pages/Index.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import {
+    useLoaderData,
+} from "react-router-dom";
+
 
 function Index() {
+    const backendStatus = useLoaderData();
+    console.log("backendStatus: ", backendStatus);
+
     return (
         <div>Index</div>
     )

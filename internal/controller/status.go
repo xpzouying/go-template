3aed460e0ae5a5c86323a3d4ee6191acdd9a4de1
zy <xpzouying@gmail.com>
2024-04-09 11:36:08 +0800

    add http middleware

---
+++ internal/controller/status.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/xpzouying/go-cmd-project-template/internal/constant"
+)
+
+type RespStatus struct {
+	Version     string `json:"version"`
+	BuildTime   string `json:"build_time"`
+	GitBranch   string `json:"git_branch"`
+	GitRevision string `json:"git_revision"`
+}
+
+// HandleGetStatus handles the GET /status API.
+func HandleGetStatus(c *gin.Context) {
+
+	WriteSuccessResponse(c, &RespStatus{
+		Version:     constant.Version,
+		BuildTime:   constant.BuildTime,
+		GitBranch:   constant.GitBranch,
+		GitRevision: constant.GitRevision,
+	})
+}
